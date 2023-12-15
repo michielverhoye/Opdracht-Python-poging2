70545bb1d04e481904828e7978cc4f6f18464342
majkull13 <r0880611@student.vives.be>
2023-12-15 12:32:30 +0100

    Auto functionaliteit werkt, begonnen met dynosheet functionaliteit

--- Classes/Garage.py
+++ Classes/Garage.py
@@ -1,15 +1,23 @@
 # Classes/garage.py
 from Database.AutoCommands import AutoCommands
+from Database.DinoSheetCommands import DinoSheetCommands
 
 class Garage:
     def __init__(self):
         self.auto_commands = AutoCommands()
+        self.dynoSheet_commands = DinoSheetCommands()
 
     def view_all_cars(self):
         all_cars = self.auto_commands.get_all_cars()
         print("All Cars:")
         for car in all_cars:
             print(car)
+
+    def view_all_dinoSheets(self):
+        all_dinoSheets = self.dynoSheet_commands.view_all_dyno_sheets()
+        print("All Dino Sheets:")
+        for dinoSheet in all_dinoSheets:
+            print(dinoSheet)
 
     def park_car(self, merk, model, kilometers, prod_year, dyno_sheet_id=None):
         self.auto_commands.add_car(merk, model, kilometers, prod_year, dyno_sheet_id)
@@ -19,3 +27,12 @@ class Garage:
 
     def drive_car(self, car_id, additional_kilometers):
         self.auto_commands.add_kilometers(car_id, additional_kilometers)
+
+    def create_dyno_sheet(self, auto_id, max_hp, max_torque, gear, fuel):
+        self.dyno_commands.create_dyno_sheet(auto_id, max_hp, max_torque, gear, fuel)
+    
+    def update_dyno_sheet(self, dyno_sheet_id, max_hp=None, max_torque=None, gear=None, fuel=None):
+        self.dyno_commands.update_dyno_sheet(dyno_sheet_id, max_hp, max_torque, gear, fuel)
+    
+    def get_dyno_sheet_by_id(self, dyno_sheet_id):
+        return self.dyno_commands.get_dyno_sheet_by_id(dyno_sheet_id)
---
+++ Database/DinoSheetCommands.py
@@ -0,0 +1,82 @@
+import sqlite3
+
+class DynoSheetCommands:
+    def __init__(self, auto_commands, database_name='Database/GarageDb.db'):
+        self.database_name = database_name
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.auto_commands = auto_commands
+
+    def disconnect(self):
+        self.conn.close()
+
+    def create_dyno_sheet(self, auto_id, max_hp, max_torque, gear, fuel):
+        # Check if the provided auto_id exists in the Auto table
+        auto = self.auto_commands.get_car_by_id(auto_id)
+        if not auto:
+            print(f"Error: Auto with ID {auto_id} not found.")
+            return
+
+        query = """
+            INSERT INTO "DynoSheet" ("MaxHp", "MaxTorque", "Gear", "Fuel", "AutoId")
+            VALUES (?, ?, ?, ?, ?)
+        """
+        self.cursor.execute(query, (max_hp, max_torque, gear, fuel, auto_id))
+        self.conn.commit()
+        print("DynoSheet created successfully.")
+
+    def update_dyno_sheet(self, dyno_sheet_id, max_hp=None, max_torque=None, gear=None, fuel=None):
+        # Check if the provided dyno_sheet_id exists
+        dyno_sheet = self.get_dyno_sheet_by_id(dyno_sheet_id)
+        if not dyno_sheet:
+            print(f"Error: DynoSheet with ID {dyno_sheet_id} not found.")
+            return
+
+        # Construct the update query based on provided parameters
+        update_query = "UPDATE DynoSheet SET "
+        update_params = []
+
+        if max_hp is not None:
+            update_query += "MaxHp = ?, "
+            update_params.append(max_hp)
+
+        if max_torque is not None:
+            update_query += "MaxTorque = ?, "
+            update_params.append(max_torque)
+
+        if gear is not None:
+            update_query += "Gear = ?, "
+            update_params.append(gear)
+
+        if fuel is not None:
+            update_query += "Fuel = ?, "
+            update_params.append(fuel)
+
+        # Remove the trailing comma and space
+        update_query = update_query.rstrip(', ')
+
+        # Add the WHERE clause
+        update_query += " WHERE Id = ?"
+        update_params.append(dyno_sheet_id)
+
+        # Execute the update query
+        self.cursor.execute(update_query, tuple(update_params))
+        self.conn.commit()
+        print(f"DynoSheet with ID {dyno_sheet_id} updated successfully.")
+
+    def get_dyno_sheet_by_id(self, dyno_sheet_id):
+        query = 'SELECT * FROM "DynoSheet" WHERE "Id" = ?'
+        self.cursor.execute(query, (dyno_sheet_id,))
+        dyno_sheet = self.cursor.fetchone()
+        return dyno_sheet
+
+    def view_all_dyno_sheets(self):
+        query = 'SELECT * FROM "DynoSheet"'
+        self.cursor.execute(query)
+        dyno_sheets = self.cursor.fetchall()
+
+        if not dyno_sheets:
+            print("No dyno sheets available.")
+        else:
+            for dyno_sheet in dyno_sheets:
+                print(dyno_sheet)
